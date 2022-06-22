2ee6d42552797c0ca5f4b105070925efd4f16441
bzhou7855 <zw7855@163.com>
2022-06-22 11:38:24 +0800

    init

---
+++ src/main/java/com/dream/Student.java
@@ -0,0 +1,39 @@
+package com.dream;
+
+public class Student {
+    private String name;
+    private Integer age;
+    private Integer id;
+
+    public Student(String name, Integer age, Integer id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+}
